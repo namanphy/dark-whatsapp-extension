3d01db420b01953fd2c10b79993e9609322261cc
namanphy <namanphy@gmail.com>
2020-06-28 21:05:45 +0530

    remove clutter | v - 0.2

--- content.js
+++ content.js
@@ -2,8 +2,3 @@ var my_body = document.body;
 if (my_body.className == 'web'){
   my_body.className = 'web dark'
 }
-// for (var i = 0, l = classes.length; i < l; i++) {
-//   if(classes[i] == ''){
-
-//   }
-// }
