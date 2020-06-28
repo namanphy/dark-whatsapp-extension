2f75ab4e00868f0d2a5675eebfd7464a3cc355cb
namanphy <namanphy@gmail.com>
2020-06-28 15:49:30 +0530

    first commit

---
+++ content.js
@@ -0,0 +1,9 @@
+var my_body = document.body;
+if (my_body.className == 'web'){
+  my_body.className = 'web dark'
+}
+// for (var i = 0, l = classes.length; i < l; i++) {
+//   if(classes[i] == ''){
+
+//   }
+// }
