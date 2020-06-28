baef49fb5492cc1dede282f64eae78c3308c9df7
namanphy <namanphy@gmail.com>
2020-06-28 22:44:57 +0530

    fixed toggle button script run, added icon

--- popup.js
+++ popup.js
@@ -1,44 +1,26 @@
 document.addEventListener('DOMContentLoaded', function() {
     var button = document.getElementById('darkInput');
-    console.log(button.value)
-    // button.addEventListener('click',   
+    console.log(button.value)  
     button.onclick = function toggle(event) {
         // console.log(event)
         if (button.value == "TOGGLE DARK THEME") {
             // console.log("OFF")
             button.value = "TOGGLE LIGHT THEME";
-            var my_body = document.body;
-            if (my_body.className == 'web'){
-                my_body.className = 'web dark'
-                }
+            chrome.tabs.executeScript({
+                code: `var my_body = document.body;
+                if (my_body.className == 'web'){
+                    my_body.className = 'web dark'
+                    }`
+            })
         } else {
             // console.log("ON")
             button.value = "TOGGLE DARK THEME";
-            var my_body = document.body;
-            if (my_body.className == 'web dark'){
-                my_body.className = 'web'
-                }
+            chrome.tabs.executeScript({
+                code: `var my_body = document.body;
+                if (my_body.className == 'web dark'){
+                    my_body.className = 'web'
+                    }`
+            })
         }
       }
-    
-    
-    // function() {
-  
-    //   chrome.tabs.getSelected(null, function(tab) {
-    //     d = document;
-    //     var flag = 0
-  
-    //     // var f = d.createElement('form');
-    //     // f.action = 'http://gtmetrix.com/analyze.html?bm';
-    //     // f.method = 'post';
-    //     // var i = d.createElement('input');
-    //     // i.type = 'hidden';
-    //     // i.name = 'url';
-    //     // i.value = tab.url;
-    //     // f.appendChild(i);
-    //     // d.body.appendChild(f);
-    //     // f.submit();
-    //   });
-    // }
-    // , false);
   }, false);
